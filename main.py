753175794421bbc8fd6251981290f814e4a092a2
moakod <moakod93@gmail.com>
2022-05-20 15:52:05 +0100

    The initial state of the script

---
+++ main.py
@@ -0,0 +1,22 @@
+from netmiko import ConnectHandler
+
+f = open('/home/moakod/BackupConfigs/routers.txt')
+IP_Addresses = f.read().splitlines()
+
+for IP in IP_Addresses:
+    Device = {
+        'device_type': 'cisco_ios',
+        'host': IP,
+        'username': 'moakod',
+        'password': 'cisco'
+    }
+
+    ssh = ConnectHandler(**Device)
+    print('Backing up device ' + str(IP))
+    ssh.send_command('terminal length 0')
+    run_config = ssh.send_command('show run')
+
+    f = open('/home/backups/Device ' + str(IP), 'w')
+    f.write(run_config)
+    f.close()
+
